0831983b3cefa17d936562f26cef3b35a1eebbdc
Tharani <tharum2002@gmail.com>
2024-08-02 19:08:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function playgame() {
+  document.getElementById("battle").innerHTML =
+    '<iframe src="https://www.onlinegames.io/games/2024/unity/drift-king/index.html" title="Chess Game"></iframe>';
+}
